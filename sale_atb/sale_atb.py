ea2be591d79c97c972c0ff3bcd5e70e0c4e97968
vovachka-coder <volodabogdan867@gmail.com>
2020-08-08 16:28:42 +0300

    add ...

--- sale_atb/sale_atb.py
+++ sale_atb/sale_atb.py
@@ -9,9 +9,10 @@ soup = BeautifulSoup(page.content, features='html.parser')
 
 def Products():
     allProducts = soup.find_all('div', class_='promo_info')
-    resultJson = []
-    resultJson.append({
-        'shopName': soup.find('div', class_='hot_line_block').text[39:-14]  # search for the store name on the page
+    result = []
+    result.append({
+        'shopName': soup.find('div', class_='hot_line_block').text[39:-14].replace(" ", '')
+        # search for the store name on the page                 #the replace method is used for good text design
 
     })
     for i in allProducts:
@@ -29,18 +30,21 @@ def Products():
 
         nameProduct = i.find('span', class_='promo_info_text').get_text(strip=True)
         newPrice = i.find('div', class_='promo_price').text[:-7]
-        resultJson.append({
+        result.append({
             'promotions_information': [{
                 'productName': nameProduct,
                 'oldPrice': price,
-                'newPrice': str(newPrice) + "." + i.find('div', class_='promo_price').find('span').text,
+                'newPrice': str(newPrice).replace(' ', '').replace('\n', '') + "." + i.find('div',
+                                                                                            class_='promo_price').find(
+                    'span').text,
+                # the replace method is used for good text design
                 # that the new price came out in a format for example 20.00 instead of 2000
                 'discount': str(discount),
             }]
 
         })
-        
-    return resultJson
+
+    return result
 
 
 start = time.perf_counter()
