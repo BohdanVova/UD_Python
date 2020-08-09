b4b513b97ff4df98df1418b8147306af8062b124
vovachka-coder <volodabogdan867@gmail.com>
2020-08-09 17:28:01 +0300

    add ...

--- sale_atb/sale_atb.py
+++ sale_atb/sale_atb.py
@@ -8,12 +8,9 @@ soup = BeautifulSoup(page.content, features='html.parser')
 
 def main():
     allProducts_disc = soup.find_all('div', class_='promo_info')
-    result = []
-    result.append({
-        'shopName': soup.find('div', class_='hot_line_block').text[39:-14].strip()
-        # search for the store name on the page                 #the replace method is used for good text design
-
-    })
+    result = {}
+    result['shopName'] = soup.find('div', class_='hot_line_block').text[39:-14].strip()
+    results = []
     for i in allProducts_disc:
         price = i.find('span', class_='promo_old_price')
         if price:
@@ -29,23 +26,22 @@ def main():
 
         nameProduct = i.find('span', class_='promo_info_text').get_text(strip=True)
         newPrice = i.find('div', class_='promo_price').text[:-7]
-        result.append({
-            'promotions_information': [{
-                'productName': nameProduct,
-                'oldPrice': price,
-                'newPrice': str(newPrice).strip() + "." + i.find('div',
-                                                                                            class_='promo_price').find(
-                    'span').text,
-                # the replace method is used for good text design
-                # that the new price came out in a format for example 20.00 instead of 2000
-                'discount': str(discount).strip(),
-            }]
+        results.append({
+
+            'productName': nameProduct,
+            'oldPrice': price,
+            'newPrice': str(newPrice).strip() + "." + i.find('div',
+                                                             class_='promo_price').find(
+                'span').text,
+
+            'discount': str(discount).strip(),
 
         })
+    result['promotions'] = results
 
     with open('Products.json', 'w') as jsonFile:
         json.dump(result, jsonFile)
 
+
 if __name__ == "__main__":
     main()
-
